210a47fcf7d3e29302f969c38f1a552bf3ed5088
diego <xing@fictionpress.com>
2011-12-08 10:52:29 +0800

    From this point forward, everything will be based on RHEL 6's gcc. Will not create compat changes for lesser gcc versions.
    Revert "COMPAT FIX: remove FORCE_INCLINE macro def. Does not work with RHEL 5 GCC. RHEL 6 is fine. "
    
    This reverts commit b47f80a03e77e9356bcad227812025ace875ad1d.

--- MurmurHash3.cpp
+++ MurmurHash3.cpp
@@ -77,12 +77,12 @@ inline uint64_t rotl64 ( uint64_t x, int8_t r )
 // Block read - if your platform needs to do endian-swapping or can only
 // handle aligned reads, do the conversion here
 
-uint32_t getblock ( const uint32_t * p, int i )
+FORCE_INLINE uint32_t getblock ( const uint32_t * p, int i )
 {
   return p[i];
 }
 
-uint64_t getblock ( const uint64_t * p, int i )
+FORCE_INLINE uint64_t getblock ( const uint64_t * p, int i )
 {
   return p[i];
 }
@@ -90,7 +90,7 @@ uint64_t getblock ( const uint64_t * p, int i )
 //-----------------------------------------------------------------------------
 // Finalization mix - force all bits of a hash block to avalanche
 
-uint32_t fmix ( uint32_t h )
+FORCE_INLINE uint32_t fmix ( uint32_t h )
 {
   h ^= h >> 16;
   h *= 0x85ebca6b;
@@ -103,7 +103,7 @@ uint32_t fmix ( uint32_t h )
 
 //----------
 
-uint64_t fmix ( uint64_t k )
+FORCE_INLINE uint64_t fmix ( uint64_t k )
 {
   k ^= k >> 33;
   k *= BIG_CONSTANT(0xff51afd7ed558ccd);
